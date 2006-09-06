1b2d1775a5a1addeb960a8d957939ad212b51f81
Nathan Gray <n8gray@n8gray.org>
2006-09-06 16:18:26 -0700

    "Package FMN" generates src tarball if darcs installed
    Also added quotes in package-fmn.sh for added robustness.
    
    darcs-hash:20060906231826-759cc-2941e66c100221927018ec0e638af3ee4851139a.gz

--- package-fmn.sh
+++ package-fmn.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
 
 version=`echo FMN_VERSION | cpp -C -include version.h -P | grep '\.'`
-fmnv=Forget-Me-Not-$version
-tmpdir=$CONFIGURATION_TEMP_DIR/$fmnv
+fmnv="Forget-Me-Not-$version"
+tmpdir="$CONFIGURATION_TEMP_DIR/$fmnv"
+export PATH=$PATH:/usr/local/bin:/sw/bin:/opt/local/bin
 
 if [ "$CONFIGURATION" != "Release" ]; then
     echo "Must use \"Release\" configuration for this target!"
@@ -11,16 +12,20 @@ fi
 
 case $ACTION in
     build)
-        rm -rf $tmpdir $TARGET_BUILD_DIR/$fmnv.tgz
-        mkdir $tmpdir
-        ln -fs $TARGET_BUILD_DIR/Forget-Me-Not.prefpane $tmpdir
-        ln -fs $SRCROOT/{README.txt,LICENSE.txt} $tmpdir
-        cd $tmpdir/..
-        tar chvzf $TARGET_BUILD_DIR/$fmnv.tgz $fmnv
+        rm -rf "$tmpdir" "$TARGET_BUILD_DIR/$fmnv.tgz"
+        mkdir "$tmpdir"
+        ln -fs "$TARGET_BUILD_DIR/Forget-Me-Not.prefpane" "$tmpdir"
+        ln -fs "$SRCROOT/README.txt" "$SRCROOT/LICENSE.txt" "$tmpdir"
+        cd "$tmpdir/.."
+        tar chvzf "$TARGET_BUILD_DIR/$fmnv.tgz" "$fmnv"
+        
+        darcs --version || exit 0
+        darcs dist "--repodir=$SRCROOT" "--dist-name=$fmnv-src"
+        mv "$SRCROOT/$fmnv-src.tar.gz" "$TARGET_BUILD_DIR/$fmnv-src.tgz"
     ;;
     
     clean)
-        rm -rf $tmpdir $TARGET_BUILD_DIR/$fmnv.tgz    
+        rm -rf "$tmpdir" "$TARGET_BUILD_DIR/$fmnv.tgz"
     ;;
     
     *)
